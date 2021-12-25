1b8111cfc76aaee5422a34c940d5768c65506cc9
deepali <deepaalipatil@gmail.com>
2021-12-25 16:19:26 +0530

    ADDED: Ability to create a BST as given in fig.

--- src/com/bridgelabz/BST/MyBinaryTree.java
+++ src/com/bridgelabz/BST/MyBinaryTree.java
@@ -2,36 +2,74 @@ package com.bridgelabz.BST;
 
 public class MyBinaryTree<K extends Comparable<K>> {
 	
-	private MyBinaryNode<K> root;
+	static MyBinaryNode root;
 	
 	 public void add(K key) {
 		 this.root = this.addRecursively(root, key);
 	 }
 
-	private MyBinaryNode<K> addRecursively(MyBinaryNode<K> current, K key) {
-		if(current == null)
-			return new MyBinaryNode<>(key);
-		int compareResult = key.compareTo(current.key);
-		if(compareResult == 0) return current;
+	private MyBinaryNode<K> addRecursively(MyBinaryNode<K> root, K key) {
+		if(root == null) {
+			root = new MyBinaryNode<>(key);
+		return root;
+	}
+		int compareResult = key.compareTo(root.key);
+		if(compareResult == 0) return root;
 		if(compareResult < 0) {
-			current.left = addRecursively(current.left, key);
+			root.left = addRecursively(root.left, key);
 		}
 		else {
-			current.right = addRecursively(current.right, key);
+			root.right = addRecursively(root.right, key);
 		}
-		return current;
+		return root;
 	}
+	
+	/** preOrder traversal :- root , left , right **/
+	 void printPreorder(MyBinaryNode node) {
+		if(node == null) {
+			return;
+		}
+		System.out.print(node.key + "  ");
+		printPreorder(node.left);
+		printPreorder(node.right);
+	}
+	 
+	void printInorder(MyBinaryNode node)
+    {
+        if (node == null)
+            return;
+        printInorder(node.left);
+        System.out.print(node.key + " ");
+        printInorder(node.right);
+    }
 	  
+	
+	 void printPostorder(MyBinaryNode node)
+	    {
+	        if (node == null)
+	            return;
+	        printPostorder(node.left);
+	        printPostorder(node.right);
+	        System.out.print(node.key + " ");
+	    }
+	 
+	    void printPostorder() { printPostorder(root); }
+	    void printInorder() { printInorder(root); }
+	    void printPreorder() { printPreorder(root); }
+	 
+	 
 	public int getSize() {
 		return this.getSizeRecursive(root);
 	}
 
-	private int getSizeRecursive(MyBinaryNode<K> current) {
+	private int getSizeRecursive(MyBinaryNode<K> root) {
 		
-		return current == null ? 0 : 1 + this.getSizeRecursive(current.left)
-		                               + this.getSizeRecursive(current.right);
+		return root == null ? 0 : 1 + this.getSizeRecursive(root.left)
+		                               + this.getSizeRecursive(root.right);
 	
-	}    
+	}
+
+	
 	
 	
 }	 
--- src/com/bridgelabz/BST/MyBinaryTreeMain.java
+++ src/com/bridgelabz/BST/MyBinaryTreeMain.java
@@ -6,8 +6,31 @@ public class MyBinaryTreeMain {
 		myBinaryTree.add(56);
 		myBinaryTree.add(30);
 		myBinaryTree.add(70);
+		myBinaryTree.add(22);
+		myBinaryTree.add(40);
+		myBinaryTree.add(11);
+		myBinaryTree.add(3);
+		myBinaryTree.add(16);
+		myBinaryTree.add(3);
+		myBinaryTree.add(60);
+		myBinaryTree.add(95);
+		myBinaryTree.add(65);
+		myBinaryTree.add(63);
+		myBinaryTree.add(67);
 		
-		System.out.println(myBinaryTree.getSize());
+		
+		System.out.print(" Pre-Order Traversal:- ");
+		myBinaryTree.printPreorder();	
+		
+		System.out.print("\n InOrder Traversal:- ");
+		myBinaryTree.printInorder();
+		
+		
+		System.out.print("\n PostOrder Traversal:- ");
+		myBinaryTree.printPostorder();
+		
+		
+		System.out.println("\n size of binaryTree:" +  myBinaryTree.getSize());
 		
 	}
 
